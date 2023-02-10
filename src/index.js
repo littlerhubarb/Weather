473de75536ddbbbbab2eabf2cb922cff39c343fa
littlerhubarb <109243033+littlerhubarb@users.noreply.github.com>
2023-02-10 14:24:31 +0100

    added forecast drafr

--- src/index.js
+++ src/index.js
@@ -24,6 +24,36 @@ let day = days[now.getDay()];
 
 h2.innerHTML = `${day}, ${date} ${month} ${year} ${hour}:${minutes}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon",];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   console.log(response.data.name);
@@ -90,3 +120,4 @@ let celsiuslink = document.querySelector("#celcius-link");
 celsiuslink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Amsterdam");
+displayForecast();
